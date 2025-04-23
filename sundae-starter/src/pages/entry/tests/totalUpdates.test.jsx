c45319e64d89de5d55c076d9913b6ea0836cf5ff
jvu <jvu@brreg.no>
2025-04-23 13:55:11 +0200

    add toppings test

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -13,14 +13,37 @@ test("update scoop subtotal when scoop change", async () => {
     // update vanilla scoops to 1, and check subtotal
     const vanillaInput = await screen.findByRole("spinbutton", {name: "Vanilla"})
 
+    // update vanilla scoops to 1 and check the subtotal
     await user.clear(vanillaInput);
     await user.type(vanillaInput, "1")
     expect(scoopsTotal).toHaveTextContent('2.00')
 
     // update chocolate scoops to 2 and check subtotal
     const chocolateInput = await screen.findByRole("spinbutton", {name: "Chocolate"})
-
     await user.clear(chocolateInput)
     await user.type(chocolateInput, "2")
     expect(scoopsTotal).toHaveTextContent('6.00')
+})
+
+test("update toppings subtotal when toppings change", async () => {
+    const user = userEvent;
+    render(<Options optionType={"toppings"} /> );
+
+    // make sure total starts out at $0.00
+    const toppingsTotal = screen.getByText("Toppings total: $", {exact: false})
+    expect(toppingsTotal).toHaveTextContent('0.00')
+
+    // add cherries and check subtotal
+    const cherriesCheckbox = await screen.findByRole("checkbox", {name: "Cherries"})
+    await user.click(cherriesCheckbox);
+    expect(toppingsTotal).toHaveTextContent('1.50')
+
+    // add hot fudge and check subtotal
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", {name: "Hot Fudge"})
+    await user.click(hotFudgeCheckbox)
+    expect(toppingsTotal).toHaveTextContent('3.00')
+
+    // remove hot fudge and check subtotal
+    await user.click(hotFudgeCheckbox)
+    expect(toppingsTotal).toHaveTextContent('1.50')
 })
